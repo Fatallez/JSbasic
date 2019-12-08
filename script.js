c237f78f02e5fca954cae81e69bc278d4142c572
Fatallez_VT <fatallez@rambler.ru>
2019-12-08 13:56:00 +0400

    +

--- script.js
+++ script.js
@@ -2,48 +2,63 @@
 
 // задание 1
 
-// let number = 100;
-// let i = 0;
-// let j;
+// let obj = {
+//     единицы: null,
+//     десятки: null,
+//     сотни: null,
+// };
 //
-// flag:
-// while (i < number) {
-//     i++;
-//     for (j = 2; j < i; j++) {
-//         if (i % j === 0) continue flag;
+// function toObject(number) {
+//     if (number > 999) {
+//         console.log('Выбранное число больше 999');
+//         return obj;
 //     }
 //
-//     console.log(i);
+//     if (number < 0) {
+//         console.log('Выбранное число меньше 0');
+//         return obj;
+//     }
+//
+//     if ((number / 100) >= 1) {
+//         obj.сотни = parseInt(number / 100);
+//     } else {
+//         obj.сотни = 0;
+//     }
+//
+//     if (number >= 100) {
+//         obj.десятки = parseInt((number - (obj.сотни * 100)) / 10);
+//     } else {
+//         obj.десятки = parseInt(number / 10);
+//     }
+//
+//     if (number >= 10) {
+//         obj.единицы = parseInt(number - 10 * obj.десятки - 100 * obj.сотни);
+//     } else {
+//         obj.единицы = number;
+//     }
+//
+//     return obj;
 // }
+//
+// toObject(512);
+// console.log(obj);
 
 // задание 2
 
-// let totalPrice = 0;
 // let basket = [
-//     ['hat', 50, 2],
-//     ['shirt', 100, 3],
-//     ['shoes', 150, 1],
-//     ['pants', 130, 2]
+//     {product: 'hat', price: 50, count: 2},
+//     {product: 'shirt', price: 100, count: 3},
+//     {product: 'shoes', price: 150, count: 1},
+//     {product: 'pants', price: 130, count: 2},
 // ];
 //
 // function countBasketPrice() {
+//     let totalPrice = 0;
 //     for (let i = 0; i < basket.length; i++) {
-//         totalPrice += basket[i][1] * basket[i][2];
+//         totalPrice += basket[i].price * basket[i].count;
 //     }
-//     console.log(totalPrice);
+//     return(totalPrice);
 // }
 //
 // countBasketPrice();
 
-// задание 3
-
-// for (let i = 0; i < 9; i++, console.log(i)) {}
-
-// задание 4
-
-// let x = '';
-//
-// for (let i = 0; i < 20; i++) {
-//     x += 'X' ;
-//     console.log(x);
-// }
